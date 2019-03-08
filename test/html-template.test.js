585f54d2120c600bf920ff0c9954b7147de2deda
COLIN COUTTS <COUTTSTCOLIN@GMAIL.COM>
2019-03-07 17:43:47 -0800

    just starting tdd of template literal

--- test/html-template.test.js
+++ test/html-template.test.js
@@ -1,6 +1,6 @@
 const test = QUnit.test;
 
-QUnit.module('test suite one');
+QUnit.module('make template literal for html image list');
 
 test('time to write a test', function(assert) {
     assert.equal(true, false);
