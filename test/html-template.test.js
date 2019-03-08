1308683267961ab203f3e87539e987b27bedc17f
COLIN COUTTS <COUTTSTCOLIN@GMAIL.COM>
2019-03-08 15:21:21 -0800

    passed first test of making html template literal

--- test/html-template.test.js
+++ test/html-template.test.js
@@ -1,24 +1,22 @@
 const test = QUnit.test;
-import projects from '../data/behance-data.js';
+import behanceData from '../data/behance-data.js';
 
 QUnit.module('make template literal for html image list');
 
 export default function makeHtmlTemplate(artData) {
     const html = /*html*/`
         <li>
-        <h2>${artData.project.name}</h2>
-        <a href=${artData.project.url}>
-        console.log('hi');
-            <img src="${artData.project.covers}">
+        <h2>${artData.projects[0].name}</h2>
+        <a href="${artData.projects[0].url}">
+            <img src="${artData.projects[0].covers.original}">
         </a>
 
         <h4>Keywords</h4>
-        <p>Advertising, Photography, Visual Effects</p>
+        <p>${artData.projects[0].fields[0]}, ${artData.projects[0].fields[1]}, ${artData.projects[0].fields[2]}</p>
         <h5>Artist</h5>
-        <p>Tim Tadder</p>
+        <p>${artData.projects[0].owners[0].first_name} ${artData.projects[0].owners[0].last_name}</p>
         </li>
     `;
-
     const template = document.createElement('template');
     template.innerHTML = html;
 
@@ -29,21 +27,21 @@ export default function makeHtmlTemplate(artData) {
 
 test('create template structure', function(assert) {
     //arrange
-    const artData = projects;
-    
+    const artData = behanceData;
+
     //act
     const result = makeHtmlTemplate(artData);
     const expected = /*html*/`
         <li>
-        <h2>Water Wigs</h2>
-        <a href="http://www.behance.net/gallery/Water-Wigs/4889175">
+        <h2>Illustrations for packaging design.</h2>
+        <a href="https://www.behance.net/gallery/66573991/Illustrations-for-packaging-design">
             <img src="https://mir-s3-cdn-cf.behance.net/projects/original/e3378b66573991.Y3JvcCwxMjAyLDk0MSwwLDE4MQ.jpg">
         </a>
 
         <h4>Keywords</h4>
-        <p>Advertising, Photography, Visual Effects</p>
+        <p>Packaging, Branding, Illustration</p>
         <h5>Artist</h5>
-        <p>Tim Tadder</p>
+        <p>Natalka Dmitrova</p>
         </li>
     `;
 
@@ -54,21 +52,21 @@ test('create template structure', function(assert) {
 
 test('pass in template literal holders', function(assert) {
     //arrange
-    const artData = projects;
-    
+    const artData = behanceData;
+    console.log(artData.projects[0].covers.original);
     //act
     const result = makeHtmlTemplate(artData);
     const expected = /*html*/`
         <li>
-        <h2>Water Wigs</h2>
-        <a href="http://www.behance.net/gallery/Water-Wigs/4889175">
+        <h2>Illustrations for packaging design.</h2>
+        <a href="https://www.behance.net/gallery/66573991/Illustrations-for-packaging-design">
             <img src="https://mir-s3-cdn-cf.behance.net/projects/original/e3378b66573991.Y3JvcCwxMjAyLDk0MSwwLDE4MQ.jpg">
         </a>
 
         <h4>Keywords</h4>
-        <p>Advertising, Photography, Visual Effects</p>
+        <p>Packaging, Branding, Illustration</p>
         <h5>Artist</h5>
-        <p>Tim Tadder</p>
+        <p>Natalka Dmitrova</p>
         </li>
     `;
 
