b73642588fb123fcae9b517fb6c22c455b5387b0
COLIN COUTTS <COUTTSTCOLIN@GMAIL.COM>
2019-03-09 15:25:45 -0800

    have some information rendering correctly but not all, small wins ya know

--- src/template-component.js
+++ src/template-component.js
@@ -1,33 +1,45 @@
-export default function makeHtmlTemplate(artData) {
-
-    for(let i = 0; i < artData.projects.length; i++){
+export default function makeHtmlTemplate(artDatas) {
+    const artData = artDatas.projects;
+    artData.forEach(artData => {
+        console.log(artData);
+        const name = artData.name;
+        console.log(name);
+        const url = artData.url;
+        const covers = artData.covers.original;
+        const fields = artData.fields;
+        const firstName = artData.owners.display_name;
+        console.log(firstName);
+        // const lastName = artData.owners.last_name;
 
         const html = /*html*/`
             <li>
-            <h2>${artData.projects[i].name}</h2>
-            <a href="${artData.projects[i].url}">
-                <img src="${artData.projects[i].covers.original}">
+            <h2>${name}</h2>
+            <a href="${url}">
+                <img src="${covers}">
             </a>
     
             <h4>Keywords</h4>
-            <p>${artData.projects[i].fields[i]}, ${artData.projects[i].fields[i + 1]}, ${artData.projects[i].fields[i + 2]}</p>
-            <h5>Artist</h5>
-            <p>${artData.projects[i].owners[i].first_name} ${artData.projects[i].owners[i].last_name}</p>
-            </li>
-        `;
-
+            ${fields.map(fields => {
+                return /*html*/`
+                    <p>${fields}</p>
+                    <h5>Artist</h5>
+                    `;
+                })}
+                <p>${firstName}</p>
+                </li>
+        `;         
+        const imageList = document.getElementById('image-list');
         const template = document.createElement('template');
         template.innerHTML = html;
-        return template.content;
-    }
-
-}
-
-const imageList = document.getElementById('image-list');
-
-export function loadArtGallery(artArray){
-    artArray.forEach(artProject => {
-        const dom = makeHtmlTemplate(artProject);
+        const dom = template.content;
         imageList.appendChild(dom);
     });
 }
+
+
+// export function loadArtGallery(artArray){
+//     artArray.forEach(artProject => {
+//         const dom = makeHtmlTemplate(artProject);
+//         imageList.appendChild(dom);
+//     });
+// }
